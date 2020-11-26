d337aef9cdaa36aa73b13bc0163f17d0e6b7adc7
Hireki <imhireki@gmail.com>
2020-11-26 19:08:02 -0300

    base project changes

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+
+def letras_diferentes(request):
+    return render(request, 'pages/letrasDiferentes.html')
