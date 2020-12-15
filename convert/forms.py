0af26386d3a4beca2ed0f84b13e9b9bcb6b00490
Hireki <imhireki@gmail.com>
2020-12-15 18:26:35 -0300

    add dark-mode

--- convert/forms.py
+++ convert/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class InputForms(forms.Form):
-    input_txt = forms.CharField(widget=forms.Textarea(attrs={"rows":5}), required=False)
+    input_txt = forms.CharField(widget=forms.Textarea(attrs={"rows":5}), required=False, label='')
     
