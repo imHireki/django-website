ae8c385fd003df648c8434fb5b5d824183f40f9d
Hireki <imhireki@gmail.com>
2020-12-22 17:00:10 -0300

    modify forms

--- convert/forms.py
+++ convert/forms.py
@@ -5,5 +5,8 @@ class InputForms(forms.Form):
     input_txt = forms.CharField(
         widget=forms.Textarea(
             attrs={
-                "rows":5, "placeholder": 'Digite seu texto aqui :D', "autofocus": True
-                }), required=False, label='', )
+                "rows":5,
+                "placeholder": 'Digite seu texto aqui :D',
+                "autofocus": True
+                }
+            ), required=False, label='', )
