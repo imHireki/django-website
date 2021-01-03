7cb980cb395aeeb55da44b16b31548deac5ca718
Hireki <imhireki@gmail.com>
2021-01-02 21:41:16 -0300

    add hr tag

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'home.apps.HomeConfig',
+    'home.apps.HomeConfig',
     'emoji.apps.EmojiConfig',
     'emoticon.apps.EmoticonConfig',
     'convert.apps.ConvertConfig',
