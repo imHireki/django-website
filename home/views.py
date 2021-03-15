a95185ce74dcaaeadd9e6b67bfd4b9fa02c74ec9
Hireki <imhireki@gmail.com>
2021-03-15 18:37:24 -0300

    add home class based view

--- home/views.py
+++ home/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 
-def home(request):
-    links = {
-    'nav_emoji': '/en/emoji',
-    'nav_convert': '/en/convert-case',
-    'nav_emoticon': '/en/emoticon'
-    }
-    transl = {
-        'name_c': 'CONVERT CASE',
-        'dev': 'Developed with \U0001F499 by Hireki',
-        'desc': 'Find here emojis, emoticons, kaomoji and convert your text for different letter cases or fancy fonts'
-        }
-    return render(request, 'home/home.html', {
-        'links': links, 'transl': transl})
+class Home(View):
+    template_name = 'home/home.html'
     
+    def get(self, request, *args, **kwargs):
+        return render(self.request, self.template_name)
