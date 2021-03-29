8f72669ca52c800271a85eabc313b630cd63314c
Hireki <imhireki@gmail.com>
2021-03-29 18:49:36 -0300

    splits emoticon template

--- emoticon/views.py
+++ emoticon/views.py
@@ -4,6 +4,14 @@ from django.views.generic.base import View
 
 class Emoticon(View):
     template_name = 'emoticon/emoticon.html'
-    
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+
+        if self.request.path == '/pt/emoticon/':
+            self.template_name = 'emoticon/emoticon_pt.html'
+
+        self.renderizar = render(self.request, self.template_name)
+
     def get(self, request, *args, **kwargs):
         return render(self.request, self.template_name)
