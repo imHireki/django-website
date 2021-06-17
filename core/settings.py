05a1df116a988048ee514c34dfdc7d6fd59c5663
imHireki <imhireki@gmail.com>
2021-06-17 20:21:53 -0300

    remove debug

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n51kidpd*slf)o_85sayx9#eu7#892efec%!x-s@w_aqiw+w=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'en.shadesapps.com',
