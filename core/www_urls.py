7fa23d80f28cf1305bcadfed1d75db663265d922
imHireki <imhireki@gmail.com>
2021-06-18 12:40:30 -0300

    add sitemap and robots txt to www

--- core/www_urls.py
+++ core/www_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import Cover
+from . import views
 
 
 urlpatterns = [
-    path('', Cover.as_view(), name='cover')
+    path('', Cover.as_view(), name='cover'),
+    path('sitemap.xml', views.SiteMap.as_view(), name='sitemap'),
+    path('robots.txt', views.RobotsTxt.as_view(), name='robots'),
 ]
